48028a8adaefdcc86116f73fb2b07fb4e6316316
maxonclaxon <satamaks@gmail.com>
2024-08-14 10:57:37 +0300

    dev

--- src/pages/personal/SchedulePage.js
+++ src/pages/personal/SchedulePage.js
@@ -1,14 +1,21 @@
 import PersonalDefaultPage from "./PersonalDefaultPage";
 import {useSelector} from "react-redux";
 import {
-    OnestBoldBig,
+    OnestBoldBig, OnestBoldDefault,
     OnestBoldMed,
     OnestNormal, OnestNormalBig,
     OnestNormalDefault,
-    OnestNormalMed
+    OnestNormalMed, OnestNormalSmall, OnestSemiBoldDefault
 } from "../../components/styled/TextComponents";
-import {color_black, color_grey_dark, color_grey_light, color_white} from "../../constants/colors";
-import {BsArrowLeft, BsArrowRight} from "react-icons/bs";
+import {
+    color_black, color_blue, color_blue_light, color_green, color_green_light,
+    color_grey_dark,
+    color_grey_light,
+    color_red_default,
+    color_red_light, color_red_ultra_light,
+    color_white, color_yellow, color_yellow_light
+} from "../../constants/colors";
+import {BsArrowLeft, BsArrowRight, BsArrowsAngleExpand, BsArrowsExpand, BsClock, BsDoorOpen} from "react-icons/bs";
 
 const ScheduleHeader = () => {
     return (
@@ -80,12 +87,151 @@ const ScheduleHeader = () => {
     )
 }
 
-// const ScheduleDay
+const ScheduleDayheader = ({weekday, lessonsCount}) => {
+    return (
+        <div style={{
+            display: "flex",
+            flexDirection: "row",
+            // justifyContent: "space-between",
+            width: "100%"
+        }}>
+            <OnestBoldDefault>{weekday}</OnestBoldDefault>
+            <OnestNormalDefault>*</OnestNormalDefault>
+            <OnestNormalDefault>{lessonsCount} урока</OnestNormalDefault>
+        </div>
+    )
+}
+
+const ScheduleDayBodyHeader = ({day}) => {
+    return (
+        <div style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between"
+        }}>
+            <OnestBoldBig>{day}</OnestBoldBig>
+            <div style={{
+                width: 20,
+                height: 20,
+                backgroundColor: color_red_default,
+                color: color_white,
+                borderRadius: "50%",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                alignContent: "center",
+                fontSize: 13
+            }}>
+                <BsArrowsAngleExpand/>
+            </div>
+        </div>
+    )
+}
+
+const LessonPreview = ({lessonData}) => {
+    const lessonColor = getLessonColor(lessonData.paymentStatus);
+    const lessonStatusColor = getLessonStatusColor(lessonData.paymentStatus);
+
+    function getLessonColor(paymentStatus){
+        if (paymentStatus === "Не оплачено") {
+            return color_red_ultra_light
+        } else if (paymentStatus === "Последний урок") {
+            return color_yellow_light
+        } else if (paymentStatus === "Оплачено") {
+            return color_green_light
+        } else {
+            return color_blue_light
+        }
+    }
+
+    function getLessonStatusColor(paymentStatus) {
+        if (paymentStatus === "Не оплачено") {
+            return color_red_light
+        } else if (paymentStatus === "Последний урок") {
+            return color_yellow
+        } else if (paymentStatus === "Оплачено") {
+            return color_green
+        } else {
+            return color_blue
+        }
+    }
+
+
+
+    return (
+        <div style={{
+            width: "100%",
+            backgroundColor: lessonColor,
+            display: "flex",
+            flexDirection: "column",
+            borderRadius: 12,
+            padding: 10,
+            boxSizing: "border-box",
+            gap: 3
+        }}>
+            <OnestSemiBoldDefault>{lessonData.studentName}</OnestSemiBoldDefault>
+            <OnestNormalDefault style={{color: lessonStatusColor}}>{lessonData.paymentStatus}</OnestNormalDefault>
+            <div style={{
+                width: "90%",
+                height: 28,
+                backgroundColor: color_white,
+                borderRadius: 10,
+                display: "flex",
+                flexDirection: "row"
+            }}>
+                <BsClock/>
+                <OnestNormalSmall>{lessonData.timeSlot}</OnestNormalSmall>
+            </div>
+        </div>
+    )
+}
+
+const ScheduleDayBody = ({day, lessons}) => {
+    return (
+        <div style={{
+            display: "flex",
+            flexDirection: "column",
+            backgroundColor: color_white,
+            width: "100%",
+            height: "100%",
+            gap: 5,
+            overflow: "scroll",
+            padding: 10
+
+        }}>
+            <ScheduleDayBodyHeader day={day}/>
+            <div style={{flexGrow: 1}}/>
+            {lessons.map(el=><LessonPreview lessonData={el} />)}
+        </div>
+    )
+}
+
+const ScheduleDay = ({weekday, day, lessons}) => {
+    return (
+        <div style={{
+            display: "flex",
+            flexDirection: "column",
+            width: 146,
+            height: 781,
+
+        }}>
+            <ScheduleDayheader weekday={weekday} lessonsCount={lessons.length}/>
+            <div style={{
+                width: "100%",
+                height: "100%",
+                backgroundColor: color_white
+            }}>
+                <ScheduleDayBody day={day} lessons={lessons}/>
+            </div>
+        </div>
+    )
+}
 
 const ScheduleCalendar = ({schedule}) => {
     return (
         <div>
-
+            <ScheduleDay weekday={"ПН"} day={"01"} lessons={schedule}/>
         </div>
     )
 }
@@ -101,6 +247,7 @@ export const SchedulePage = () => {
                 width: "100%"
             }}>
                 <ScheduleHeader/>
+                <ScheduleCalendar schedule={schedule}/>
             </div>
         </PersonalDefaultPage>
     )
